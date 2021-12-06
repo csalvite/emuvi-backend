c2b705e72a79724dadf54d16837469fdc2ef8f58
Danutty <danielsecolozano@gmail.com>
2021-12-06 16:45:57 +0100

    editUser terminado

--- controllers/user/editUser.js
+++ controllers/user/editUser.js
@@ -30,6 +30,8 @@ const editUser = async (req, res, next) => {
 
     let message = 'Usuario actualizado con exito';
 
+    //Email
+
     //en caso de que obtengamos un mail, comprobamos si es distinto al existente
 
     if (newEmail && newEmail !== user[0].email) {
@@ -38,10 +40,48 @@ const editUser = async (req, res, next) => {
         [newEmail]
       );
       // si el email existe lanzamos un error
+      if (email.leght > 0) {
+        const error = new Error(
+          'Ya existe un usuario con ese email en la base de datos'
+        );
+        error.httpStatus = 409;
+        throw error;
+      }
+      //creamos codigo de registro  de un solo uso
+      const registrationCode = generateRandomString(40);
+
+      // actualizamos el usuario junto con el codigo de registro
+
+      await connection.query(
+        `UPDATE users SET email = ?, registrationCode = ?, active = false, modifiedAt = ?
+        WHERE id = ?`,
+        [newEmail, registrationCode, new Date(), idUser]
+      );
+
+      // enviamos mail de confirmacion al nuevo email registrado
+      await verifyEmail(newEmail, registrationCode);
+
+      //Actualizamos el mensage que enviaremos al "res.send".
+      message += ', comprueba tu nuevo mail para activarlo';
     }
+    //NOMBRE
+
+    //En caso de que haya nombre comprobamos si es distinto al existente
+
+    if (username && username !== user[0].name) {
+      `UPDATE users SET username = ?, modifiedAt = ? where id = ?`,
+        [username, new Date(), idUser];
+    }
+
+    res.send({
+      status: 'ok',
+      message,
+    });
   } catch (error) {
     next(error);
   } finally {
     if (connection) connection.release();
   }
 };
+
+module.exports = editUser;
