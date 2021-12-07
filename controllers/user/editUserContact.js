3ab4ba28179077894b8d16d166d71f3aecae8245
alexcoba <alexcobavazquez.hotmail.com>
2021-12-07 12:55:47 +0100

    create edituserContact. and add savephoto en helpers

---
+++ controllers/user/editUserContact.js
@@ -0,0 +1,95 @@
+const { getConnection } = require('../../db');
+const { savePhoto } = require('../../helpers');
+
+async function editUser(req, res, next) {
+  let connection;
+  try {
+    connection = await getConnection();
+
+    const { username, name, lastname, email, biography, birthday } = req.body;
+
+    //comprobamos que siempre exista email y contraseña
+    if (!password || !email)
+      throw new Error('El nombre de usuario y el email son obligatorios');
+
+    //comprobamos que no exista un usuario con el mismo email
+    const [existingUser] = await connection.query(
+      `
+            SELECT *
+            FROM user
+            WHERE email=? AND NOT id=?
+            `,
+      [email, req.isauth.id]
+    );
+
+    //comprobamos que el nuevo email no coincida con uno ya existente
+    if (existingUser.length > 0)
+      throw new Error('Ya existe un usuario con este email');
+
+    //CAMBIO DE IMAGEN PERFIL
+    //actualiza el perfil si incluye el avatar
+    if (req.files) {
+      let nameFile;
+      try {
+        nameFile = await savePhoto({
+          file: req.files.avatar,
+          directory: 'avatar',
+        });
+      } catch (error) {
+        throw new Error('No se ha podido subir la imagen');
+      }
+
+      //actualiza los datos de usuario incluyendo el avatar
+      await connection.query(
+        `
+            UPDATE user
+            SET avatar=?,
+                username=?,
+                name=?,
+                lastname=?,
+                email=?,
+                biography=?,
+                birthday=?
+            WHERE id=?
+            `,
+        [
+          nameFile,
+          username,
+          name,
+          lastname,
+          email,
+          biography,
+          birthday,
+          req.isauth.id,
+        ]
+      );
+
+      //actualizar usuario si no cambia la img
+    } else {
+      await connection.query(
+        `
+            UPDATE user
+            SET username=?,
+                name =?,
+                lastname =?,
+                email=?,
+                biography=?,
+                birthday=?
+            WHERE id=?
+            `,
+        [username, name, lastname, email, biography, birthday, req.isauth.id]
+      );
+    }
+
+    res.send({
+      status: 'OK',
+      message: 'Datos de usuario modificados con éxito',
+    });
+  } catch (error) {
+    next(error);
+  } finally {
+    if (connection) connection.release();
+  }
+}
+
+module.exports = { editUserContact };
--- helpers.js
+++ helpers.js
@@ -4,6 +4,8 @@
 const crypto = require('crypto');
 const sgMail = require('@sendgrid/mail');
 const path = require('path');
+const uuid = require('uuid');
+const { ensureDir, unlink } = require('fs-extra');
 
 //Importar las variables de entorno necesarias
 const { SENDGRID_API_KEY, SENDGRID_FROM, PUBLIC_HOST, UPLOADS_DIRECTORY } =
@@ -86,10 +88,54 @@ async function deletePhoto(photoName) {
     throw new Error('Error al eliminar la imagen del servidor');
   }
 }
+/**
+ * ###############
+ * ## savePhoto ##
+ * ###############
+ */
+async function savePhoto(image, type) {
+  try {
+    // Comprobamos que el directorio de subida de imágenes exista.
+    await ensureDir(uploadsDir);
+
+    // Convertimos la imagen en un objeto "Sharp".
+    const sharpImage = sharp(image.data);
+
+    // Accedemos a los metadatos de la imagen para posteriormente comprobar
+    // el ancho total.
+    const imageInfo = await sharpImage.metadata();
+
+    // Si el tipo de imagen es 0 (avatar) redimensionamos la imagen a 150x150.
+    if (type === 0) {
+      sharpImage.resize(150, 150);
+    }
+
+    // Si la imagen es de tipo 2 (entrada) y el ancho supera el máximo indicado
+    // redimensionamos la imagen.
+    else if (type === 1 && imageInfo.width > 1000) {
+      sharpImage.resize(1000);
+    }
+
+    // Generamos un nombre único para la imagen.
+    const imageName = `${uuid.v4()}.jpg`;
+
+    // Creamos la ruta absoluta a la ubicación donde queremos guardar la imagen.
+    const imagePath = path.join(uploadsDir, imageName);
+
+    // Guardamos la imagen en el directorio "uploads".
+    await sharpImage.toFile(imagePath);
+
+    // Retornamos el nombre del fichero.
+    return imageName;
+  } catch (_) {
+    throw new Error('Error al procesar la imagen');
+  }
+}
 
 module.exports = {
   generateRandomString,
   sendMail,
   verifyEmail,
   deletePhoto,
+  savePhoto,
 };
