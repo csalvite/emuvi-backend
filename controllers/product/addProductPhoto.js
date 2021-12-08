af67d10d1f60aebfdf340bb92de03c39eaa71942
alexcoba <alexcobavazquez.hotmail.com>
2021-12-08 19:28:32 +0100

    Add addProductPhoto.js in folder Controllers

--- controllers/product/addProductPhoto.js
+++ controllers/product/addProductPhoto.js
@@ -1,12 +1,41 @@
+const { concurrency } = require('sharp');
 const getDB = require('../../database/getDB');
 const { savePhoto } = require('../../helpers');
 const addProductPhoto = async (req, res, next) => {
-    let connection;
-try {
-     connection = await getDB();
+  let connection;
+  try {
+    connection = await getDB();
     const { idProduct } = req.params;
-    //Si no hay foto, casca
-    
-} catch (error) {
-    
-}
+    //Si no hay foto, error.
+    if (!(req.files || req.files.avatar)) {
+      const error = new Error('Faltan campos');
+      error.httpStatus = 400;
+      throw error;
+    }
+    //Vamos a conseguir las fotos de los productos
+    const [photos] = await connection.query(
+      `SELECT id FROM product_photo WHERE idProduct = ?`,
+      [idProduct]
+    );
+    // Si hay mas de 5 lanzamos
+    if (photos.length >= 5) {
+      const error = new Error('Esta entrada ya tiene las 5 fotos permitidas');
+      error.httpStatus = 403;
+      throw error;
+    }
+    //obtenemos el nombre de la foto de la base de datos.
+    const photoName = await savePhoto(req.files.photo);
+    await connection.query(
+      `INSERT INTO product_photo (name, idProduct) VALUES (?, ?)`,
+      [photoName, idProduct]
+    );
+    res.send({
+      status: 'ok',
+      message: 'La foto ha sido añadida correctamente.',
+    });
+  } catch (error) {
+    next(error);
+  } finally {
+    if (connection) connection.release();
+  }
+};
