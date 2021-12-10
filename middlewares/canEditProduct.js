c0f804b28cd8bf9a5cd4f461a450584e6a8aa7b7
csalvite <csalvite@gmail.com>
2021-12-10 17:20:16 +0100

    editProduct tested

--- middlewares/canEditProduct.js
+++ middlewares/canEditProduct.js
@@ -1,19 +1,27 @@
+// Middleware que comprueba si el usuario es el propietario del producto
 const getDB = require('../database/getDB');
+
 const canEditProduct = async (req, res, next) => {
   let connection;
   try {
+    connection = await getDB();
+
     const { idProduct } = req.params;
+
     const idReqUser = req.userAuth.id;
+
     //Ahora buscamos el id del usuario del producto
     const [idUserowner] = await connection.query(
       `SELECT idUser FROM product WHERE id = ?`,
       [idProduct]
     );
+
     if (idUserowner[0].idUser !== idReqUser) {
       const error = new Error('No tienes permisos para editar este producto');
       error.httpStatus = 403;
       throw error;
     }
+
     next();
   } catch (error) {
     next(error);
