cdce9c2deb87e87e7a9f6e0ac9d1427782c2ec08
csalvite <csalvite@gmail.com>
2021-12-10 18:36:20 +0100

    prettier config modified

---
+++ controllers/offers/deniedOffer.js
@@ -0,0 +1,15 @@
+const getDB = require('../../database/getDB');
+
+const deniedOffer = async (req, res, next) => {
+  let connection;
+
+  try {
+    connection = await getDB();
+  } catch (error) {
+    next(error);
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+module.exports = deniedOffer;
