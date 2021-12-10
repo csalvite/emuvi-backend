25f7b999b29e59463eb223f5c8f1a1bbb0daaab2
alexcoba <alexcobavazquez.hotmail.com>
2021-12-10 20:22:39 +0100

    add addFavProduct.js and deleteFavProducts.js

--- controllers/favorite/addFavProduct.js
+++ controllers/favorite/addFavProduct.js
@@ -1,2 +1,32 @@
 const getDB = require('../../database/getDB');
-async function addFavProduct(req, res, next)
+async function addFavProduct(req, res, next) {
+    let connection;
+    try {
+        connection = await getDB();
+        const { idProduct } = req.params;
+        const idReqUser = req.userAuth.id;
+        const [Productexits] = await connection.query(
+            `
+            SELECT * FROM user_favorite_product WHERE idUser = ? AND idProduct = ?`,
+            [req.auth.id, idProduct]
+        );
+        if (favexits.length > 0) {
+            const error = new Error('Ya tienes ese producto en tus favoritos');
+            error.httpStatus = 404;
+            throw error;
+        }
+        const [result] = await connection.query(
+            `INSERT INTO user_favorite_product(idUser, idProduct) VALUES(?, ?)`,
+            [req.auth.id, , idProduct]
+        );
+        res.send({
+            status: 'ok',
+            message: 'Producto añadido como favorito correctamente',
+        });
+    } catch (error) {
+        next(error);
+    } finally {
+        if (connection) connection.release();
+    }
+}
+module.exports = { addFavProduct };
--- controllers/favorite/deleteFavProducts.js
+++ controllers/favorite/deleteFavProducts.js
@@ -1,26 +1,38 @@
 const getDB = require('../../database/getDB');
 
 const deleteFavProduct = async (req, res, next) => {
-  try {
-    Connection = await getDB();
+    try {
+        Connection = await getDB();
 
-    const { idProduct } = req.params;
+        const { idProduct } = req.params;
 
-    const idReqUser = req.userAuth.id;
+        const idReqUser = req.userAuth.id;
 
-    await connection.query(`DELETE FROM user_favorite_product WHERE id = ?`, [
-      idProduct,
-    ]);
+        const [favProduct] = await connection.query(
+            `SELECT * FROM user_favorite_product WHERE idUser = ?
+     AND id_product = ?`,
+            [req.auth.id, idProduct]
+        );
+        if (favProduct < 1) {
+            const error = new Error('No tienes el producto en tus favoritos');
+            error.httpStatus = 404;
+            throw error;
+        }
 
-    res.send({
-      status: 'ok',
-      message: 'Producto eliminado de tu lista de favoritos',
-    });
-  } catch (error) {
-    next(error);
-  } finally {
-    if (connection) connection.release();
-  }
+        await connection.query(
+            `DELETE FROM user_favorite_product WHERE idUser = ? AND idProduct = ?`,
+            [idProduct, idReqUser]
+        );
+
+        res.send({
+            status: 'ok',
+            message: 'Producto eliminado de tu lista de favoritos',
+        });
+    } catch (error) {
+        next(error);
+    } finally {
+        if (connection) connection.release();
+    }
 };
 
 module.exports = deleteFavProduct;
