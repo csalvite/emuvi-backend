1b3c7514e1da5fe72dca7499c493b3fe1c6a1c9f
alexcoba <alexcobavazquez.hotmail.com>
2021-12-12 17:34:58 +0100

    add index.js in folder votes

---
+++ controllers/votes/index.js
@@ -0,0 +1,9 @@
+const newVote = require('./newVote');
+const deleteVote = require('./deleteVote');
+const editVote = require('./editVote');
+
+module.exports = {
+    newVote,
+    deleteVote,
+    editVote,
+};
