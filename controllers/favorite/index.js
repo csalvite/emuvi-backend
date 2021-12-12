8f845653762f839434795a89168fcb15175b96c3
alexcoba <alexcobavazquez.hotmail.com>
2021-12-12 17:41:22 +0100

    add index.js in folder favorite

---
+++ controllers/favorite/index.js
@@ -0,0 +1,8 @@
+const addFavProduct = require('./addFavProduct');
+const deleteFavProducts = require('./deleteFavProducts');
+const listUserFavProducts = require('./listUserFavProducts');
+module.exports = {
+    addFavProduct,
+    deleteFavProducts,
+    listUserFavProducts,
+};
